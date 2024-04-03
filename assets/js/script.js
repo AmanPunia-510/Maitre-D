6796b90252cb9665bbf846459a5b8b6d449f3d44
AmanPunia-510 <puniaaman888@gmail.com>
2024-04-03 21:14:12 +0530

    slick slider section complete with responsive view

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: false,
+    speed: 300,
+    prevArrow: ".left-svg",
+    nextArrow: ".right-svg",
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
