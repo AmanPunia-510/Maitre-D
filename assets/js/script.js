a55f8bc916c97742defa0576228f110d3ce46981
AmanPunia-510 <puniaaman888@gmail.com>
2024-04-04 21:33:31 +0530

    work on funtionality of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,47 @@ $('.slider').slick({
     infinite: true,
     autoplay: false,
     speed: 300,
-    prevArrow: ".left-svg",
-    nextArrow: ".right-svg",
+    prevArrow: ".prev",
+    nextArrow: ".next",
     slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 992,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                autoplay: true,
                 infinite: true,
                 dots: false
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 576,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                autoplay: true,
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
     ]
 });
+
+// onscroll animation
+
+window.addEventListener("scroll", () => {
+    let all_items = document.querySelectorAll(".animation-part");
+
+    all_items.forEach(items => {
+        let top = items.getBoundingClientRect().top;
+
+        if (top < window.innerHeight) {
+            items.classList.add("opacity");
+        }
+        else {
+            items.classList.remove("opacity");
+        }
+    })
+})
+
+// work on searchbar
