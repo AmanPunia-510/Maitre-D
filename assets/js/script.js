badc5b948ae8e41bd52a8e1a20c4acb2bccf9474
AmanPunia-510 <puniaaman888@gmail.com>
2024-04-05 10:44:18 +0530

    work in progress of animation and funtioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,21 +29,82 @@ $('.slider').slick({
     ]
 });
 
-// onscroll animation
+ AOS.init();
 
-window.addEventListener("scroll", () => {
-    let all_items = document.querySelectorAll(".animation-part");
+const search_svg = document.querySelector(".s-svg");
+const search_box = document.querySelector(".search-box");
 
-    all_items.forEach(items => {
-        let top = items.getBoundingClientRect().top;
+search_svg.addEventListener("click", () => {
+    if (search_box.classList.contains("d-none")) {
+        search_box.classList.remove("d-none");
+        search_box.classList.add("d-block");
+    }
 
-        if (top < window.innerHeight) {
-            items.classList.add("opacity");
-        }
-        else {
-            items.classList.remove("opacity");
-        }
-    })
+    else {
+        search_box.classList.add("d-none");
+        search_box.classList.remove("d-block");
+    }
 })
 
-// work on searchbar
+const keywords = [
+    'About Us',
+    'Features',
+    'Modules',
+    'Simple pricing for everyone',
+    'Get in touch!',
+];
+
+const result = document.querySelector("#result-here");
+const search = document.querySelector("#search");
+
+search.addEventListener("keyup", () => {
+    let filteredResults = [];
+    let input = search.value.toLowerCase();
+    if (input.length) {
+        filteredResults = keywords.filter((keyword) => {
+            return keyword.toLowerCase().includes(input);
+        });
+        displayResults(filteredResults);
+    } else {
+        result.textContent = '';
+    }
+});
+
+function displayResults(filteredResults) {
+    result.innerHTML = ''; 
+    if (filteredResults.length > 0) {
+        filteredResults.forEach((item) => {
+            const listItem = document.createElement('div');
+            listItem.textContent = item;
+            result.appendChild(listItem);
+        });
+    } else {
+        result.textContent = 'No matching results found';
+    }
+}
+
+
+// work on toggle
+
+const toggle = document.querySelector(".toggle");
+const toggleBall = document.querySelector(".toggle-ball");
+const right1 = document.querySelector(".right1-svg");
+const right2 = document.querySelector(".right2-svg");
+const txt_449 = document.querySelector(".txt-449");
+const month = document.querySelector(".month");
+
+toggle.addEventListener("click", () => {
+    toggleBall.classList.toggle("right");
+
+    if (toggleBall.classList.contains("right")) {
+        right1.setAttribute("src", "./assets/images/right.png");
+        right2.setAttribute("src", "./assets/images/right.png");
+        txt_449.innerHTML = "$900";
+        month.innerHTML = "/year";
+    } else {
+        right1.setAttribute("src", "./assets/images/wrong.png");
+        right2.setAttribute("src", "./assets/images/wrong.png");
+        txt_449.innerHTML = "$449";
+        month.innerHTML = "/Month";
+    }
+});
